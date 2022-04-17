da86a0fe4e1f7472327348bb34b82c7ccdfea71f
Ruslan Abdulaev <abdylaevrr@gmail.com>
2022-04-17 23:50:42 +0300

    Update number_guess.py
    Add useful comments

--- number_guess.py
+++ number_guess.py
@@ -4,7 +4,7 @@ num = random.randint(1, 100)
 
 print('Добро пожаловать в числовую угадайку')
 
-
+# Check validity of the input number
 def is_valid(n):
     if n.isdigit() is True and 1 <= int(n) <= 100:
         return True
@@ -13,10 +13,16 @@ def is_valid(n):
 
 
 while True:
+    
+    # User inputs a number
     n = input('Введите число от 1 до 100')
+    
+    # Use defined above function to check user's input
     if is_valid(n) is False:
         print('А может быть все-таки введем целое число от 1 до 100?')
         continue
+    
+    # if everything is OK check if user guesses the number
     else:
         n = int(n)
         if n < num:
@@ -27,5 +33,5 @@ while True:
         else:
             print('Вы угадали, поздравляем!')
             break
-
+            
 print('Спасибо, что играли в числовую угадайку. Еще увидимся...')
